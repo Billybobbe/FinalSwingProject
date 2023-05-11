3e52e66226982d613241bc8c2025b35f95459cae
David <24dmontgomery@athenian.org>
2023-05-10 17:23:22 -0700

    Added movement and started on basic collisions.

---
+++ src/Projectile.java
@@ -0,0 +1,7 @@
+import java.awt.*;
+
+public class Projectile extends Sprite{
+    public Projectile(Image i, int x, int y, int width, int height, int speedX, int speedY) {
+        super(i, x, y, width, height, speedX, speedY);
+    }
+}
